1796ad7e8b247d27c11192526466e2eb5f75d028
aaronpenne <aaronpenne@users.noreply.github.com>
2018-04-09 08:17:43 -0700

    Using participant_id as dict key directly.

--- hangouts_json_parser.py
+++ hangouts_json_parser.py
@@ -18,7 +18,6 @@ input_file = os.path.join('data_private', 'Hangouts.json')
 output_dir = os.path.relpath('output')
 if not os.path.isdir(output_dir):
     os.mkdir(output_dir)
-output_file = os.path.join(output_dir, 'Hangouts')
     
 # With json module
 with open(input_file, 'r', encoding="utf8") as f:
@@ -33,46 +32,56 @@ for i in range(num_conversations):
     name = ''
     message = ''
     
+
     
-    with open(os.path.join(output_dir, 'Hangouts_{:03}.txt'.format(i)), 'a+') as f:
-    
-        # Extract info from metadata
-        conversation = data['conversations'][i]['conversation']['conversation']
-        num_participants = len(conversation['participant_data'])
-        for j in range(num_participants):
-            participant[j] = {}
-            # Get participant name
-            try:
-                participant[j]['name'] = conversation['participant_data'][j]['fallback_name']
-            except:
-                participant[j]['name'] = 'Anonymous'
-            
-            # Get participant ID
-            try:
-                participant[j]['id'] = conversation['participant_data'][j]['id']['chat_id']
-            except:
-                participant[j]['id'] = 'Anon_ID'
+    # Extract info from metadata
+    conversation = data['conversations'][i]['conversation']['conversation']
+    num_participants = len(conversation['participant_data'])
+    for participant_index in range(num_participants):
+        # Get participant ID
+        try:
+            participant_id = conversation['participant_data'][participant_index]['id']['chat_id']
+        except:
+            participant_id = 'Anon_ID_{}'.format(participant_index)
+        
+        # Get participant name
+        try:
+            participant_name = conversation['participant_data'][participant_index]['fallback_name']
+        except:
+            participant_name = 'Anon_{}'.format(participant_index)
+        
+        if participant_name == 'Aaron Penne':
+            participant_name = 'Me'
+        participant[participant_id] = participant_name
                 
-        # Extract info from messages
-        events = data['conversations'][i]['events']
-        num_events = len(events)
-        for j in range(num_events):
-            try:
-                # Just grab text for now # FIXME add more?
-                if events[j]['chat_message']['message_content']['segment'][0]['type'] == 'TEXT':
-               
+    names = ''    
+    for key in participant:
+        names = '{} {}'.format(names, participant[key]) 
+    output_file_log = os.path.join(output_dir, 'Hangouts {:03}{}.txt'.format(i, names))
+    output_file_tsv = os.path.join(output_dir, 'Hangouts {:03}{}.tsv'.format(i, names))
+    print(output_file_log)
+    with open(output_file_log, 'w+') as f_log:
+        with open(output_file_tsv, 'w+') as f_tsv:
+        
+            # Extract info from messages
+            events = data['conversations'][i]['events']
+            num_events = len(events)
+            for j in range(num_events):
+                try:           
                     timestamp = int(events[j]['timestamp'])/1000000
                     timestamp = datetime.fromtimestamp(timestamp).strftime('%m/%d/%Y %H:%M:%S')
-                    name = events[j]['sender_id']['chat_id']
+                    name = participant[events[j]['sender_id']['chat_id']]
                     
-                    # FIXME Add support for more than one line (#include stdio.h, etc.)
-                    message = events[j]['chat_message']['message_content']['segment'][0]['text'] 
-            except:
-                continue
+                    segment = events[j]['chat_message']['message_content']['segment']
+                    for k in range(len(segment)):
+                        message = segment[k]['text'].replace('\n', ' ').replace('\t', ' ')
+                        message_type = segment[k]['type']
+                        if timestamp and name and message_type != 'LINE_BREAK':
+                            f_log.write('[{}] <{}> {}\n'.format(timestamp, name, message))
+                            f_tsv.write('{}\t{}\t{}\n'.format(timestamp, name, message))
+    
+                except:
+                    continue
             
-            try:
-                f.write('[{}] <{}> {}\n'.format(timestamp, name, message))
-#                print('[{}] <{}> {}\n'.format(timestamp, name, message))
-            except:
-                continue
+        
 
